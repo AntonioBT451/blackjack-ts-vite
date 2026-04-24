8ff3f17734115afe163a2205527aa19f50bb767d
AntonioBT451 <antoniobt451@gmail.com>
2026-04-23 19:13:00 -0600

    feat: add card drawing and card value calculation functions

--- src/blackjack/deck.ts
+++ src/blackjack/deck.ts
@@ -7,13 +7,13 @@ import _ from 'underscore';
  * 2C = Two of spades
  */
 
-let deck: string[] = [];
 const tipos = ['C', 'D', 'H', 'S'];
 const especiales = ['A', 'J', 'Q', 'K'];
 
+// Función para crear una nueva baraja
 export const crearDeck = () => {
     // Limpiar desk
-    deck = [];
+    const deck: string[] = [];
 
     // Cartas numericas (2-10)
     for (let i = 2; i <= 10; i++) {
@@ -29,8 +29,34 @@ export const crearDeck = () => {
         };
     };
 
-    deck = _.shuffle(deck);
+    const shuffleDeck = _.shuffle(deck);
 
     // 52 cartas: (9 numericas + 4 especiales) x 4 palos 
-    return deck;
+    return shuffleDeck;
 };
+
+// Función para tomar una carta
+export const pedirCarta = (deck: string[]): { carta: string, nuevaBaraja: string[] } => {
+    if (deck.length === 0) {
+        throw new Error('No hay cartas en la baraja');
+    };
+
+    const carta = deck.pop() ?? '';
+
+    return {
+        carta: carta,
+        nuevaBaraja: deck
+    };
+};
+
+// Función para calcular el valor de una carta
+export const valorCarta = (carta: string): number => {
+    const valorString = carta.substring(0, carta.length - 1);
+
+    if (isNaN(Number(valorString))) {
+        return valorString === 'A' ? 11 : 10;
+    } else {
+        return Number(valorString);
+    }
+};
+
