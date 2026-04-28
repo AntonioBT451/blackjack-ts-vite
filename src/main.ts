a7eff6c5be37e76635b20d7320bde53a691d0285
AntonioBT451 <antoniobt451@gmail.com>
2026-04-27 21:09:18 -0600

    feat(ui): implement UI functions to update scores, display cards and alert messages

--- src/main.ts
+++ src/main.ts
@@ -1,2 +1,45 @@
-// src/main.ts
-console.log('¡Proyecto con Vite + TypeScript funcionando!');
+import { crearDeck, pedirCarta, valorCarta } from "./blackjack/deck";
+import { actualizarPuntos, mostrarCarta, mostrarMensaje } from "./blackjack/render";
+import type { JuegoState } from "./blackjack/types";
+
+const juegoState: JuegoState = {
+    baraja: [],
+    jugador: {
+        puntos: 0,
+        cartas: [],
+    },
+    computadora: {
+        puntos: 0,
+        cartas: []
+    },
+    juegoTerminado: false
+}
+
+// Referencias a elementos DOM
+const btnPedir = document.querySelector('#btnPedir')! as HTMLButtonElement;
+
+juegoState.baraja = crearDeck();
+console.log(juegoState.baraja);
+
+// Funciones
+const handlePedirCarta = () => {
+    const { carta, nuevaBaraja } = pedirCarta(juegoState.baraja);
+
+    juegoState.baraja = nuevaBaraja;
+    juegoState.jugador.puntos = juegoState.jugador.puntos + valorCarta(carta);
+
+    actualizarPuntos('#ptosJugador', juegoState.jugador.puntos);
+    mostrarCarta(carta);
+
+    if (juegoState.jugador.puntos > 21) {
+        mostrarMensaje('Lo siento, perdiste.', 'warning');
+        btnPedir.disabled = true;
+    } else if (juegoState.jugador.puntos === 21) {
+        mostrarMensaje('¡21, ganaste!', 'success');
+        btnPedir.disabled = true;
+    }
+};
+
+
+// Event listeners
+btnPedir?.addEventListener('click', handlePedirCarta);
