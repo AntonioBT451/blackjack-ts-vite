38decb425e03baa577d4153eec99a7e6dbb8ac24
AntonioBT451 <antoniobt451@gmail.com>
2026-04-28 12:45:40 -0600

    feat: implement blackjack game logic with computer turn

--- src/main.ts
+++ src/main.ts
@@ -2,8 +2,8 @@ import { crearDeck, pedirCarta, valorCarta } from "./blackjack/deck";
 import { actualizarPuntos, mostrarCarta, mostrarMensaje } from "./blackjack/render";
 import type { JuegoState } from "./blackjack/types";
 
-const juegoState: JuegoState = {
-    baraja: [],
+let juegoState: JuegoState = {
+    baraja: crearDeck(),
     jugador: {
         puntos: 0,
         cartas: [],
@@ -17,29 +17,92 @@ const juegoState: JuegoState = {
 
 // Referencias a elementos DOM
 const btnPedir = document.querySelector('#btnPedir')! as HTMLButtonElement;
-
-juegoState.baraja = crearDeck();
-console.log(juegoState.baraja);
+const btnDetener = document.querySelector('#btnDetener')! as HTMLButtonElement;
 
 // Funciones
+const deshabilitarBotones = () => {
+    btnPedir.disabled = true;
+    btnDetener.disabled = true;
+}
+
+// Funciones jugador
 const handlePedirCarta = () => {
+    if (juegoState.juegoTerminado) return;
+
     const { carta, nuevaBaraja } = pedirCarta(juegoState.baraja);
 
     juegoState.baraja = nuevaBaraja;
     juegoState.jugador.puntos = juegoState.jugador.puntos + valorCarta(carta);
+    juegoState.jugador.cartas.push(carta);
 
     actualizarPuntos('#ptosJugador', juegoState.jugador.puntos);
-    mostrarCarta(carta);
+    mostrarCarta(carta, "#cartas-jugador");
 
-    if (juegoState.jugador.puntos > 21) {
-        mostrarMensaje('Lo siento, perdiste.', 'warning');
-        btnPedir.disabled = true;
-    } else if (juegoState.jugador.puntos === 21) {
-        mostrarMensaje('¡21, ganaste!', 'success');
-        btnPedir.disabled = true;
+    if (juegoState.jugador.puntos >= 21) {
+        juegoState.juegoTerminado = true;
+
+        deshabilitarBotones();
+        turnoComputadora(juegoState.jugador.puntos);
+        mostrarGanador();
     }
 };
+
+const handleDetener = () => {
+    if (juegoState.juegoTerminado) return;
+
+    juegoState.juegoTerminado = true;
+
+    deshabilitarBotones();
+    turnoComputadora(juegoState.jugador.puntos);
+    mostrarGanador();
+};
+
+
+// Funciones computadora
+const turnoComputadora = (puntosMinimos: number) => {
+
+    do {
+        const { carta, nuevaBaraja } = pedirCarta(juegoState.baraja);
+
+        juegoState.baraja = nuevaBaraja;
+        juegoState.computadora.puntos += valorCarta(carta);
+        juegoState.computadora.cartas.push(carta);
+
+        actualizarPuntos('#ptosComputadora', juegoState.computadora.puntos);
+        mostrarCarta(carta, "#cartas-computadora");
+    } while ((juegoState.computadora.puntos < puntosMinimos) && puntosMinimos <= 21);
+};
+
+// Funciones
+const mostrarGanador = () => {
+    const puntosJugador = juegoState.jugador.puntos;
+    const puntosComputadora = juegoState.computadora.puntos;
+
+    if (puntosJugador === 21 && puntosComputadora !== 21) {
+        mostrarMensaje('¡Blackjack! ¡Felicidades, ganaste!', 'success');
+        return;
+    }
+
+    if (puntosJugador > 21) {
+        mostrarMensaje('Lo siento, perdiste.', 'warning');
+        return;
+    }
+
+    if (puntosComputadora > 21) {
+        mostrarMensaje('¡Ganaste! La computadora se pasó de 21.', 'warning');
+        return;
+    }
+
+    if (puntosJugador > puntosComputadora) {
+        mostrarMensaje('¡Felicidades, ganaste!', 'warning');
+    } else if (puntosJugador < puntosComputadora) {
+        mostrarMensaje('La computadora gana. ¡Suerte la próxima!', 'warning');
+    } else {
+        mostrarMensaje('Empate!', 'warning');
+    }
+}
 
 
 // Event listeners
 btnPedir?.addEventListener('click', handlePedirCarta);
+btnDetener?.addEventListener('click', handleDetener);
